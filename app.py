894f3b17f1b0d8fe0b5ff27857528020a4462983
Leidy Dorado <doradoleidypao@gmail.com>
2021-09-29 12:59:30 -0400

    app.py file update

--- app.py
+++ app.py
@@ -73,9 +73,10 @@ def stats(start=None,end=None):
         results = session.query(*sel).\
             filter(Measurement.date >= start).all()
         temps = list(np.ravel(results))
-    
+        return jsonify(temps)
+
     results = session.query(*sel).\
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
-    return jsonify(temps=temps)
+    return jsonify(temps)
